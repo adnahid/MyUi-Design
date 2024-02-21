3b2c3812ede8c2637adeeffd489f3ac6f1ad1565
Hp <myselfabout432@gmail.com>
2024-02-21 15:30:45 +0600

    This is simple ui

---
+++ app/src/main/java/com/example/verioustype_learing/Fragment_one.java
@@ -0,0 +1,75 @@
+package com.example.verioustype_learing;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class Fragment_one extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        CardView bangla = view.findViewById(R.id.bangla);
+        ImageSlider image_slider = view.findViewById(R.id.image_slider);
+
+//======================================ImageSlider============================================//
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel("https://i.imgur.com/DvpvklR.png", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://th.bing.com/th/id/OIP.TdWOh7jpwkPiCcayHuh6OQHaEK?rs=1&pid=ImgDetMain", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel("https://th.bing.com/th/id/OIP.OxzBr4Qh5r3_ydeK60QJngHaE-?rs=1&pid=ImgDetMain", ScaleTypes.CENTER_CROP));
+        image_slider.setImageList(slideModels);
+//======================================ImageSlider============================================//
+
+
+
+
+        // Picasso.get().load("https://i.imgur.com/DvpvklR.png").into(bi);
+        bangla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(getActivity(), topLayout.class);
+                startActivity(intent);
+
+ 
+
+            }
+        });
+
+
+
+        return view;
+
+    }
+
+
+
+}
